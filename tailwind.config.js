b3a55acaabdad1d6aedf1870afb2c592edfa4526
Natu <shreya135yadav@gmail.com>
2024-07-10 01:30:27 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          codroid_transparent: '#1e293bec',
+          codroid_blue: '#27b3df',
+          codroid_purple: '#7B68EE',
+          codroid_purple_transparent: '#7c68eecc',
+          codroid_hover_purple: '#6148f0'
+        },
+      },
+    },
+}
